54122fe0312a962218b438f9df869e238c056061
Marcus Absher <marcus.absher@gmail.com>
2025-11-10 09:24:41 -0800

    added calcuator to aiagent

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+from google.genai import types
+
+def main():
+
+    # Load environment variables from .env file
+    load_dotenv()
+
+    args = sys.argv[1:]
+    
+    is_verbose = "--verbose" in sys.argv[1:] 
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise ValueError("GEMINI_API_KEY not found in environment variables.")
+    client = genai.Client(api_key=api_key)
+    
+    user_prompt = " ".join(args)
+
+    if is_verbose:
+        print(f"User prompt: {user_prompt}\n") 
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, is_verbose)
+
+def generate_content(client, messages, is_verbose=True):
+
+    response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+    )
+    # Print the .text property of the response
+    print("--- Model Response ---")
+    print(response.text)
+
+    # Print the token usage metadata
+    if is_verbose:
+        print("\n--- Token Usage ---")
+        usage = response.usage_metadata
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
+    
+
+
+if __name__ == "__main__":
+    main()
