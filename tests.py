60f37a2164ec6a25ef55351ca3d869308afa4ac0
Marcus Absher <marcus.absher@gmail.com>
2025-11-12 11:06:32 -0800

    created run python file function and tests

--- tests.py
+++ tests.py
@@ -1,19 +1,33 @@
-from functions.write_file import write_file
-
+from  functions.run_python_file import run_python_file
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result for writing to 'lorem.txt' file:")
+    result = run_python_file("calculator", "main.py")
+    print("should print the calculator's usage instructions after running calculator's main.py file:")
     print(result)
     print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for writing to 'pkg/morelorem.txt' file:")
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print("Result for running calculator file:")
     print(result)
     print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for writing to '/tmp/temp.tx' file:")
+    result = run_python_file("calculator", "tests.py")
+    print("Result for running to 'tests.py' file:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "../main.py")
+    print("Running python file from a bad file path - this should return an error")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print("Running a non existent python file - this should return an error")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "lorem.txt")
+    print("Running file 'lorem.tx' should return error becuase it is not a python file :")
     print(result)
     print("")
 
