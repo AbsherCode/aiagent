17a24a97aef8202cc8c97b14f432644baf9c0053
Marcus Absher <marcus.absher@gmail.com>
2025-11-13 13:17:44 -0800

    created function declaration for get file info function

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from dotenv import load_dotenv
 from google import genai
 import sys
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+# Define the available functions tool list
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ])
 
 def main():
 
@@ -40,10 +57,22 @@ def generate_content(client, messages, is_verbose=True):
     response = client.models.generate_content(
             model="gemini-2.0-flash-001",
             contents=messages,
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt,
+                tools=[available_functions],
+            ),
     )
     # Print the .text property of the response
     print("--- Model Response ---")
-    print(response.text)
+    # Check for function calls first
+    function_calls = response.function_calls
+    if function_calls:
+        for function_call_part in function_calls:
+            # Print the function call
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        # If no function calls, print the text response
+        print(response.text)
 
     # Print the token usage metadata
     if is_verbose:
